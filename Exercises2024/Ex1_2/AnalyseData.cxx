2b445f61d0972a4c0517c60731a9a485ee8efe44
C282028 <c_cassells@outlook.com>
2024-11-14 14:37:19 +0000

    Assignment_1_4.3

--- Exercises2024/Ex1_2/AnalyseData.cxx
+++ Exercises2024/Ex1_2/AnalyseData.cxx
@@ -9,28 +9,25 @@
 
 using namespace std;
 
-int main()
-{
-    // Variables 
-    string line1;
-    string line2;
-    string filename = "input2D_float.txt";
-    ifstream myInput;
-    float x, y;
-    int lines = 0;
 
+vector <pair<float,float>> readCoords(string& filename)
+{
     // creates a vecotr from a pair of floats
     vector <pair<float,float>> coords;
 
+    // read variables 
+    string line1, line2;
+    float x, y;
+    int lines = 0;
+    ifstream myInput(filename);
+
     // Finds and opens txt file, checks if successful, then proceeds.
-    myInput.open("/workspaces/SUPA_CPP_Labs/Exercises2024/Ex1_2/input2D_float.txt");
     if (myInput.fail())
     {
         cout << "Unable to open file" << endl;
-        return -1;
+        return coords;
     }
-
-    else
+else
     {
         // Skips the header line (non float values)
         string header;
@@ -44,13 +41,46 @@ int main()
             x = stof(line1);
             y = stof(line2);
             coords.emplace_back(x,y);
-            cout << "(" << coords.back().first << "," << coords.back().second << ")"<< endl;
         }
     }
-    // Prints total No. of lines and closes the file.
-    cout << lines << " lines read from file" << endl;
     myInput.close();
+    return coords;
+} //readCoords
+
+void printCoords(vector<pair<float, float>>& coords, int n)
+{
+    // Print variables
+    int lines = 0;
+    int lines_to_print = n;
+
+    if (n > coords.size())
+    {
+        cout << "***Warning*** " << n << " larger than total number of coordinates" << endl;
+        cout << "Printing first 5 coordinates instead" << endl;
+        lines_to_print = min(5, static_cast<int>(coords.size()));
+    }
+
+// Print up to the specified number of lines
+	for (int i = 0; i < lines_to_print; ++i) 
+    {
+		cout << "(" << coords[i].first << ", " << coords[i].second << ")" << endl;
+	}
+	cout << lines_to_print << " lines printed from file" << endl;
+    cout << coords.size() << " lines read from file" << endl;
+
+} //printCoords
+
+int main()
+{
+    // Specify file to read from
+    string filepath = "/workspaces/SUPA_CPP_Labs/Exercises2024/Ex1_2/input2D_float.txt";
+    // Calls the reading function
+    vector<pair<float, float>> coords = readCoords(filepath);
+    // Specify the number of lines to print
+	int n = 26;
+    // Calls the printing function
+    printCoords(coords, n);
 
     return 0;
     
-} // main // namespace
+} // main
